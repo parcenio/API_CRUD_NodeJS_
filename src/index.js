0ff88a14a9536dac009c4899ceaf75a7699e50a2
Paulo Arcenio <pauloarcenio.cs@gmail.com>
2020-04-13 20:34:25 -0300

    Parâmetros: Query, Route e Request params

--- src/index.js
+++ src/index.js
@@ -1,22 +1,41 @@
 const express = require("express");
 
 const app = express();
+
+app.use(express.json());
+
 /**
- * PUT  ( Criando Informação)
+ * 
  */
+
 app.get('/projects', (request, response) => {
+  const { title, owner } = request.query;
+
+  console.log(title);
+  console.log(owner);
+
   return response.json([
     'Projeto 1', 'Projeto 2'
   ]);
 });
 
 app.post('/projects', (request, response) => {
+
+  const {title,owner} = request.body;
+
+  console.log(title);
+  console.log(owner);
+
   return response.json([
     'Projeto 1', 'Projeto 2', 'Projeto 3',
   ]);
 })
 
 app.put('/projects/:id', (request, response) => {
+  const {id} = request.params;
+
+  console.log(id);
+
   return response.json([
     'Projeto 4', 'Projeto 2', 'Projeto 3',
   ]);
