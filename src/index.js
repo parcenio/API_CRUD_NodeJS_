6e50aa183318be168b7d8e61c740d36df52c0638
Paulo Arcenio <pauloarcenio.cs@gmail.com>
2020-04-14 21:07:18 -0300

    Utilizando middlewares: logRequests and validateProjectId

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { uuid} = require('uuidv4');
+const {uuid,isUuid} = require('uuidv4');
 
 const app = express();
 
@@ -10,6 +10,36 @@ app.use(express.json());
  */
 
 const projects = [];
+
+function logRequests(request, response, next){
+  const {method, url } = request;
+
+  const logLabel = `[${method.toUpperCase()}] ${url}`;
+
+  console.log(logLabel);
+
+  return next(); //Próximo middleware
+
+}
+
+function validateProjectId(request, response, next){
+
+  const {id} = request.params;
+
+  if(!isUuid(id)) {
+    return response.status(400).json({error: "Invalid project ID."});
+  }
+
+  return next();
+
+}
+
+app.use(logRequests);
+
+/**
+ * Pode ser usado desta forma que estará abrangendo a todas rotas que atenderem aos parâmetros
+ * "... app.use('/projects/:id',validateProjectId, middleware1, middleware2,) ...""
+*/
 
 app.get('/projects', (request, response) => {
   
@@ -32,7 +62,7 @@ app.post('/projects', (request, response) => {
   return response.json(project);
 })
 
-app.put('/projects/:id', (request, response) => {
+app.put('/projects/:id', validateProjectId, (request, response) => {
   const {id} = request.params;
   const {title, owner } = request.body;
 
@@ -54,7 +84,7 @@ app.put('/projects/:id', (request, response) => {
   return response.json(project);
 })
 
-app.delete('/projects/:id', (request, response) => {
+app.delete('/projects/:id', validateProjectId, (request, response) => {
 
   const {id}=request.params;
  
